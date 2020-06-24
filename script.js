1034c553fe5d96f927e385398eaee249d3130d19
Inshal Ayaz <inshal.ayaz15@gmail.com>
2020-06-24 15:33:19 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+}
+function clearNumber(){
+    // document.getElementById('result').innerHTML = 0
+    alert(0)
+}
